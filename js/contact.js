a37ddeb8433b6f4e9f6a65537ced15761a1eeae2
omarmusa-har <omarmusa200218@gmail.com>
2020-04-17 17:28:36 +0300

    omar

--- js/contact.js
+++ js/contact.js
@@ -1,74 +1,74 @@
-// (function($) {
-//     "use strict";
+(function($) {
+    "use strict";
 
 
-//     /*==================================================================
-//     [ Validate ]*/
-//     var name = $('.validate-input input[name="name"]');
-//     var orgname = $('.validate-input input[name="orgname"]');
-//     var email = $('.validate-input input[name="email"]');
-//     var phone = $('.validate-input input[name="phone"]');
-//     var subject = $('.validate-input input[name="subject"]');
-//     var message = $('.validate-input textarea[name="message"]');
+    /*==================================================================
+    [ Validate ]*/
+    var name = $('.validate-input input[name="name"]');
+    var orgname = $('.validate-input input[name="orgname"]');
+    var email = $('.validate-input input[name="email"]');
+    var phone = $('.validate-input input[name="phone"]');
+    var subject = $('.validate-input input[name="subject"]');
+    var message = $('.validate-input textarea[name="message"]');
 
 
-//     $('.validate-form').on('submit', function() {
-//         var check = true;
+    $('.validate-form').on('submit', function() {
+        var check = true;
 
-//         if ($(orgname).val().trim() == '') {
-//             showValidate(orgname);
-//             check = false;
-//         }
+        if ($(orgname).val().trim() == '') {
+            showValidate(orgname);
+            check = false;
+        }
 
-//         if ($(name).val().trim() == '') {
-//             showValidate(name);
-//             check = false;
-//         }
-
-
-//         if ($(subject).val().trim() == '') {
-//             showValidate(subject);
-//             check = false;
-//         }
+        if ($(name).val().trim() == '') {
+            showValidate(name);
+            check = false;
+        }
 
 
-//         if ($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-//             showValidate(email);
-//             check = false;
-//         }
-
-//         // if ($(phone).val().trim().match() == '') {
-//         //     showValidate(phone);
-//         //     check = false;
-//         // }
-
-//         if ($(message).val().trim() == '') {
-//             showValidate(message);
-//             check = false;
-//         }
-
-//         return check;
-//     });
+        if ($(subject).val().trim() == '') {
+            showValidate(subject);
+            check = false;
+        }
 
 
-//     $('.validate-form .input1').each(function() {
-//         $(this).focus(function() {
-//             hideValidate(this);
-//         });
-//     });
+        if ($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check = false;
+        }
 
-//     function showValidate(input) {
-//         var thisAlert = $(input).parent();
+        // if ($(phone).val().trim().match() == '') {
+        //     showValidate(phone);
+        //     check = false;
+        // }
 
-//         $(thisAlert).addClass('alert-validate');
-//     }
+        if ($(message).val().trim() == '') {
+            showValidate(message);
+            check = false;
+        }
 
-//     function hideValidate(input) {
-//         var thisAlert = $(input).parent();
-
-//         $(thisAlert).removeClass('alert-validate');
-//     }
-
+        return check;
+    });
 
 
-// })(jQuery);
+    $('.validate-form .input1').each(function() {
+        $(this).focus(function() {
+            hideValidate(this);
+        });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+
+
+})(jQuery);
